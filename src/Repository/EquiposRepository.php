49971bc21e217fd330c701e7957c9014c3b402a2
Gaston Tomas Huete <44206933+GastonTH@users.noreply.github.com>
2022-02-15 19:03:43 +0100

    add Doctrine Extensions

--- src/Repository/EquiposRepository.php
+++ src/Repository/EquiposRepository.php
@@ -6,12 +6,29 @@ use Doctrine\ORM\EntityRepository;
 class EquiposRepository extends EntityRepository
 {
 
+    /*
+     * $this->getEntityManager()
+    ->createQuery('
+        SELECT p
+            FROM GabrielUploadBundle:Image p
+            WHERE p.upvotes > '.$maxvotes.'
+            ORDER BY p.createdAt ASC
+    ')
+    ->getSQL();*/
+
     public function findAllTeams(){
 
         $dql = "SELECT e.nombre, e.ciudad, e.conferencia, e.division FROM App:Equipos e";
         $query = $this->getEntityManager()->createQuery($dql);
 
         return $query->getArrayResult();
+
+    }
+
+    public function findTeamByName(string $nombreIn){
+
+        $query = $this->getEntityManager()->createQuery("SELECT e.nombre, e.ciudad, e.conferencia, e.division FROM Equipos:e WHEN :nombreIn");
+
 
     }
 
