7bdc322d4355353add3ff1a7230fca33013d5278
Gaston Tomas Huete <44206933+GastonTH@users.noreply.github.com>
2022-02-17 18:06:10 +0100

    rest

--- src/Controller/PartidosController.php
+++ src/Controller/PartidosController.php
@@ -2,11 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Equipos;
+use App\Entity\Partidos;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class PartidosController extends AbstractController
 {
+    public function getResultsByLocalTeam(Request $request){
 
+        $localOrVisitor = "local";
+
+        $team = $request ->get('nombre');
+        $teamObjs = $this->getDoctrine()
+            ->getManager()
+            ->getRepository(Equipos::class)
+            ->findOneBy(['nombre' => $team]);
+
+        $teamRes = $this->getDoctrine()->getRepository(Partidos::class)
+            ->findResultsByLocalTeam($teamObjs, $localOrVisitor);
+
+        return new JsonResponse($teamRes);
+
+    }
+
+    public function getResultsByVisitorTeam(Request $request){}
+    public function getMediaResultsByLocalTeam(Request $request){}
+    public function getMediaResultsByVisitorTeam(Request $request){}
 }
--- src/Repository/PartidosRepository.php
+++ src/Repository/PartidosRepository.php
@@ -1,8 +1,39 @@
 <?php
 
 namespace App\Repository;
+use App\Entity\Equipos;
 use Doctrine\ORM\EntityRepository;
 
 class PartidosRepository extends EntityRepository
 {
+
+    public function findResultsByLocalTeam(Equipos $team){
+
+        $query = $this->getEntityManager()
+            ->createQuery("SELECT p FROM App:Partidos p WHERE p.equipoLocal = :teamIn");
+        $query->setParameter('teamIn', $team->getNombre());
+
+        $res = $query->getArrayResult();
+
+        return $res;
+
+    }
+    public function findResultsByVisitorTeam(Equipos $team, string $localOrVisitor){
+
+        $query = $this->getEntityManager()
+            ->createQuery("SELECT p FROM App:Partidos p WHERE p.equipoLocal = :teamIn");
+        $query->setParameter('teamIn', $team->getNombre());
+
+        $res = $query->getArrayResult();
+
+        return $res;
+
+    }
+    public function findMediaResultsByLocalTeam(Equipos $team){
+
+    }
+    public function findMediaResultsByVisitorTeam(Equipos $team){
+
+    }
+
 }
