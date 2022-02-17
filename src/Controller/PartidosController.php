cd5d2059cb3284be9572593be92c8f7d18c9d1b8
Gaston Tomas Huete <44206933+GastonTH@users.noreply.github.com>
2022-02-17 18:59:54 +0100

    all queries done

--- src/Controller/PartidosController.php
+++ src/Controller/PartidosController.php
@@ -12,7 +12,20 @@ class PartidosController extends AbstractController
 {
     public function getResultsByLocalTeam(Request $request){
 
-        $localOrVisitor = "local";
+        $team = $request ->get('nombre');
+        $teamObjs = $this->getDoctrine()
+            ->getManager()
+            ->getRepository(Equipos::class)
+            ->findOneBy(['nombre' => $team]);
+
+        $teamRes = $this->getDoctrine()->getRepository(Partidos::class)
+            ->findResultsByLocalTeam($teamObjs);
+
+        return new JsonResponse($teamRes);
+
+    }
+
+    public function getResultsByVisitorTeam(Request $request){
 
         $team = $request ->get('nombre');
         $teamObjs = $this->getDoctrine()
@@ -21,13 +34,38 @@ class PartidosController extends AbstractController
             ->findOneBy(['nombre' => $team]);
 
         $teamRes = $this->getDoctrine()->getRepository(Partidos::class)
-            ->findResultsByLocalTeam($teamObjs, $localOrVisitor);
+            ->findResultsByVisitorTeam($teamObjs);
 
         return new JsonResponse($teamRes);
 
     }
 
-    public function getResultsByVisitorTeam(Request $request){}
-    public function getMediaResultsByLocalTeam(Request $request){}
-    public function getMediaResultsByVisitorTeam(Request $request){}
+    public function getMediaResultsByLocalTeam(Request $request){
+
+        $team = $request ->get('nombre');
+        $teamObjs = $this->getDoctrine()
+            ->getManager()
+            ->getRepository(Equipos::class)
+            ->findOneBy(['nombre' => $team]);
+
+        $teamRes = $this->getDoctrine()->getRepository(Partidos::class)
+            ->findMediaResultsByLocalTeam($teamObjs);
+
+        return new JsonResponse($teamRes);
+
+    }
+    public function getMediaResultsByVisitorTeam(Request $request){
+
+        $team = $request ->get('nombre');
+        $teamObjs = $this->getDoctrine()
+            ->getManager()
+            ->getRepository(Equipos::class)
+            ->findOneBy(['nombre' => $team]);
+
+        $teamRes = $this->getDoctrine()->getRepository(Partidos::class)
+            ->findMediaResultsByVisitorTeam($teamObjs);
+
+        return new JsonResponse($teamRes);
+
+    }
 }
--- src/Repository/PartidosRepository.php
+++ src/Repository/PartidosRepository.php
@@ -10,29 +10,60 @@ class PartidosRepository extends EntityRepository
     public function findResultsByLocalTeam(Equipos $team){
 
         $query = $this->getEntityManager()
-            ->createQuery("SELECT p FROM App:Partidos p WHERE p.equipoLocal = :teamIn");
+            ->createQuery("SELECT p  FROM App:Partidos p WHERE p.equipoLocal = :teamIn");
         $query->setParameter('teamIn', $team->getNombre());
 
-        $res = $query->getArrayResult();
+        $arrayResult[$team->getNombre()] = $query->getArrayResult();
 
-        return $res;
+        return $arrayResult;
 
     }
-    public function findResultsByVisitorTeam(Equipos $team, string $localOrVisitor){
+    public function findResultsByVisitorTeam(Equipos $team){
 
         $query = $this->getEntityManager()
-            ->createQuery("SELECT p FROM App:Partidos p WHERE p.equipoLocal = :teamIn");
+            ->createQuery("SELECT p  FROM App:Partidos p WHERE p.equipoVisitante = :teamIn");
         $query->setParameter('teamIn', $team->getNombre());
 
-        $res = $query->getArrayResult();
+        $arrayResult[$team->getNombre()] = $query->getArrayResult();
 
-        return $res;
+        return $arrayResult;
 
     }
     public function findMediaResultsByLocalTeam(Equipos $team){
 
+        $res = $this->findResultsByVisitorTeam($team);
+
+        $media = 0;
+
+        foreach ($res[$team->getNombre()] as $iter){
+
+            $media += $iter["puntosLocal"];
+        }
+
+        $media /= count($res[$team->getNombre()]);
+
+        $arrayRes[$team->getNombre()] = $media;
+
+        return $arrayRes;
+
     }
+
     public function findMediaResultsByVisitorTeam(Equipos $team){
+
+        $res = $this->findResultsByVisitorTeam($team);
+
+        $media = 0;
+
+        foreach ($res[$team->getNombre()] as $iter){
+
+            $media += $iter["puntosVisitante"];
+        }
+
+        $media /= count($res[$team->getNombre()]);
+
+        $arrayRes[$team->getNombre()] = $media;
+
+        return $arrayRes;
 
     }
 
